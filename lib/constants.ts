d6e76b015a19ad7decbf875bb83a5619717d34df
Ronna Firmo <ronnwiseley@gmail.com>
2021-09-14 10:41:12 +0800

    feat: add ability to set rate

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,4 @@
 export const k = {
   API_KEY_KEY: 'a2xha2llX2FwaV9rZXk',
+  HOURLY_RATE_KEY: 'hourly_rate',
 };
--- pages/workspace.tsx
+++ pages/workspace.tsx
@@ -1,7 +1,15 @@
 import { Avatar } from '@chakra-ui/avatar';
+import { Button, ButtonGroup } from '@chakra-ui/button';
 import { useColorMode } from '@chakra-ui/color-mode';
-import { ExternalLinkIcon, SettingsIcon } from '@chakra-ui/icons';
-import { Box, Container, Flex, Spacer, Text } from '@chakra-ui/layout';
+import { useDisclosure } from '@chakra-ui/hooks';
+import {
+  ArrowBackIcon,
+  ArrowForwardIcon,
+  EditIcon,
+  ExternalLinkIcon,
+  SettingsIcon,
+} from '@chakra-ui/icons';
+import { Box, Container, Flex, HStack, Spacer, Text } from '@chakra-ui/layout';
 import {
   Menu,
   MenuButton,
@@ -10,13 +18,27 @@ import {
   MenuItem,
   MenuList,
 } from '@chakra-ui/menu';
-import { HStack } from '@chakra-ui/react';
+import {
+  IconButton,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
 import { Select } from '@chakra-ui/select';
 import { Switch } from '@chakra-ui/switch';
+import { useToast } from '@chakra-ui/toast';
+import dayjs from 'dayjs';
+import Cookies from 'js-cookie';
 import _ from 'lodash';
 import { GetServerSidePropsContext } from 'next';
 import Head from 'next/head';
-import React from 'react';
+import numeral from 'numeral';
+import React, { useState } from 'react';
 import { k } from '../lib/constants';
 import { ClockifyUser } from '../lib/models/clockify-user';
 import { ClockifyWorkspace } from '../lib/models/clockify-workspace';
@@ -24,10 +46,41 @@ import { ClockifyWorkspace } from '../lib/models/clockify-workspace';
 interface Props {
   user: ClockifyUser;
   workspaces: ClockifyWorkspace[];
+  initialHourlyRate: number;
 }
 
 const Workspace = (props: Props) => {
   const { colorMode, toggleColorMode } = useColorMode();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const toast = useToast();
+
+  const [hourlyRate, setHourlyRate] = useState(props.initialHourlyRate);
+  const [range, setRange] = useState({
+    start: dayjs().startOf('week'),
+    end: dayjs().endOf('week'),
+  });
+
+  const onSaveHourlyRate = (e: React.SyntheticEvent) => {
+    e.preventDefault();
+
+    const target = e.target as typeof e.target & {
+      rate: { value: number };
+    };
+
+    if (!target.rate.value) {
+      return toast({
+        title: "It's blank, you dumbass.",
+        status: 'error',
+      });
+    }
+
+    Cookies.set(k.HOURLY_RATE_KEY, target.rate.value.toString());
+    setHourlyRate(target.rate.value);
+
+    onClose();
+  };
+
   return (
     <>
       <Head>
@@ -40,47 +93,113 @@ const Workspace = (props: Props) => {
       </Head>
       <Box py='8'>
         <Container maxW='container.md'>
+          {/* start::Header */}
           <Flex alignItems='center'>
             <Box>
+              <Select defaultValue={props.user.defaultWorkspace}>
+                {props.workspaces.map((workspace) => (
+                  <option key={workspace.id} value={workspace.id}>
+                    {workspace.name}
+                  </option>
+                ))}
+              </Select>
+            </Box>
+            <Spacer />
+            <Box>
               <HStack spacing='4'>
-                <Select defaultValue={props.user.defaultWorkspace}>
-                  {props.workspaces.map((workspace) => (
-                    <option key={workspace.id} value={workspace.id}>
-                      {workspace.name}
-                    </option>
-                  ))}
-                </Select>
-                <Text as='kbd'>P567.44/hr</Text>
+                <Text>Hi, {props.user.name.split(' ')[0]}!</Text>
+                <Menu placement='bottom-end' closeOnSelect={false}>
+                  <MenuButton
+                    as={Avatar}
+                    name={props.user.name}
+                    src={props.user.profilePicture}
+                  />
+                  <MenuList>
+                    <MenuGroup title='Color mode'>
+                      <MenuItem>
+                        <Switch
+                          mr='3'
+                          isChecked={colorMode === 'dark'}
+                          onChange={toggleColorMode}
+                        />
+                        <span>{_.startCase(colorMode)} mode</span>
+                      </MenuItem>
+                    </MenuGroup>
+                    <MenuDivider />
+                    <MenuItem icon={<SettingsIcon />}>Settings</MenuItem>
+                    <MenuItem icon={<ExternalLinkIcon />}>Log out</MenuItem>
+                  </MenuList>
+                </Menu>
+              </HStack>
+            </Box>
+          </Flex>
+          {/* end::Header */}
+
+          {/* start::Customizers */}
+          <Flex alignItems='center' mt='8'>
+            <Box>
+              <HStack>
+                <Text as='kbd' fontWeight='bold' fontSize='lg'>
+                  ₱{numeral(hourlyRate).format('0,0.00')} /hr
+                </Text>
+                <IconButton
+                  aria-label='Edit hourly rate'
+                  variant='ghost'
+                  icon={<EditIcon />}
+                  onClick={onOpen}
+                />
               </HStack>
             </Box>
             <Spacer />
             <Box>
-              <Menu placement='bottom-end' closeOnSelect={false}>
-                <MenuButton
-                  as={Avatar}
-                  name={props.user.name}
-                  src={props.user.profilePicture}
-                />
-                <MenuList>
-                  <MenuGroup title='Color mode'>
-                    <MenuItem>
-                      <Switch
-                        mr='3'
-                        isChecked={colorMode === 'dark'}
-                        onChange={toggleColorMode}
-                      />
-                      <span>{_.startCase(colorMode)} mode</span>
-                    </MenuItem>
-                  </MenuGroup>
-                  <MenuDivider />
-                  <MenuItem icon={<SettingsIcon />}>Settings</MenuItem>
-                  <MenuItem icon={<ExternalLinkIcon />}>Log out</MenuItem>
-                </MenuList>
-              </Menu>
+              <HStack>
+                <Select>
+                  <option value='semi-monthly'>Semi-monthly</option>
+                  <option value='weekly'>Weekly</option>
+                </Select>
+                <ButtonGroup isAttached>
+                  <IconButton
+                    aria-label='Previous date range'
+                    icon={<ArrowBackIcon />}
+                  />
+                  <Button>This week</Button>
+                  <IconButton
+                    aria-label='Next date range'
+                    icon={<ArrowForwardIcon />}
+                  />
+                </ButtonGroup>
+              </HStack>
             </Box>
           </Flex>
+          {/* end::Customizers */}
         </Container>
       </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <form onSubmit={onSaveHourlyRate}>
+          <ModalOverlay />
+          <ModalContent>
+            <ModalHeader>Edit hourly rate</ModalHeader>
+            <ModalCloseButton />
+            <ModalBody pb={6}>
+              <Input
+                name='rate'
+                type='number'
+                step='any'
+                placeholder='Enter your hourly rate'
+              />
+            </ModalBody>
+            <ModalFooter>
+              <Button mr={3} type='submit'>
+                Save
+              </Button>
+              <Button onClick={onClose} type='button'>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </ModalContent>
+        </form>
+      </Modal>
     </>
   );
 };
@@ -102,11 +221,16 @@ export async function getServerSideProps(
   const workspacesResponse = await fetch(`${baseUrl}/v1/workspaces`, init);
   const workspaces = await workspacesResponse.json();
 
+  const initialHourlyRate = parseFloat(
+    context.req.cookies[k.HOURLY_RATE_KEY] || '0'
+  );
+
   // Pass data to the page via props
   return {
     props: {
       user,
       workspaces,
+      initialHourlyRate,
     },
   };
 }
